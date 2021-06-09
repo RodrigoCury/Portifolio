9bd97323311fb9adb8c1df4fe688e47c0102b317
RodrigoCury <rodrigocury@ufu.br>
2021-06-08 21:07:14 -0300

    Add Glsl raw loader

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -96,6 +96,14 @@ module.exports = {
                                 }
                             }
                         ]
+                },
+                // GLSL
+                {
+                    test: /\.(glsl|vs|fs|vert|frag)$/,
+                    exclude: /node_modules/,
+                    use: [
+                        'raw-loader'
+                    ]
                 }
 
             ]
