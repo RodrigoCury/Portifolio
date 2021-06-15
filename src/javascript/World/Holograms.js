97a3f1933d4d2bd26e22be46a90ad125530e078b
RodrigoCury <rodrigocury@ufu.br>
2021-06-15 17:23:07 -0300

    add debug | position is container responsability

--- src/javascript/World/Holograms.js
+++ src/javascript/World/Holograms.js
@@ -39,11 +39,21 @@ export default class Holograms {
             )
             mesh.name = geometry.name
             mesh.visible = false
-            mesh.position.set(-2, 0, 4)
-            mesh.rotation.y = Math.PI / 2
 
             this.container.add(mesh)
         })
+
+        this.container.rotation.y = -1.224
+        this.container.position.set(0.266, -0.38, 6.795)
+
+        if (this.debug) {
+            this.debugFolder.add(this.container.position, 'x', -10, 10, 0.001).name("Holograms X")
+            this.debugFolder.add(this.container.position, 'y', -10, 10, 0.001).name("Holograms Y")
+            this.debugFolder.add(this.container.position, 'z', -10, 10, 0.001).name("Holograms Z")
+            this.debugFolder.add(this.container.rotation, 'x', -Math.PI, Math.PI, 0.001).name("Holograms Rot X")
+            this.debugFolder.add(this.container.rotation, 'y', -Math.PI, Math.PI, 0.001).name("Holograms Rot Y")
+            this.debugFolder.add(this.container.rotation, 'z', -Math.PI, Math.PI, 0.001).name("Holograms Rot Z")
+        }
     }
 
     setupLightCone() {
@@ -52,17 +62,17 @@ export default class Holograms {
             this.materials.items.beamMaterial
         )
         this.cone.visible = false
-        this.cone.position.set(-1.4, -0.75, 4)
-        this.cone.rotation.set(0, Math.PI, 2)
+        this.cone.position.set(5.298, -1.584, 5.297)
+        this.cone.rotation.set(-Math.PI, 0.1, -2.32)
 
 
         if (this.debug) {
-            this.debugFolder.add(this.cone.position, 'x', -5, 5, 0.01).name(`X`)
-            this.debugFolder.add(this.cone.position, 'y', -5, 5, 0.01).name(`Y`)
-            this.debugFolder.add(this.cone.position, 'z', -5, 5, 0.01).name(`Z`)
-            this.debugFolder.add(this.cone.rotation, 'x', -Math.PI, Math.PI, 0.01).name(`rot x`)
-            this.debugFolder.add(this.cone.rotation, 'y', -Math.PI, Math.PI, 0.01).name(`rot y`)
-            this.debugFolder.add(this.cone.rotation, 'z', -Math.PI, Math.PI, 0.01).name(`rot z`)
+            this.debugFolder.add(this.cone.position, 'x', -5, 6, 0.001).name(`X Cone`)
+            this.debugFolder.add(this.cone.position, 'y', -5, 6, 0.001).name(`Y Cone`)
+            this.debugFolder.add(this.cone.position, 'z', -5, 6, 0.001).name(`Z Cone`)
+            this.debugFolder.add(this.cone.rotation, 'x', -Math.PI, Math.PI, 0.01).name(`rot x Cone`)
+            this.debugFolder.add(this.cone.rotation, 'y', -Math.PI, Math.PI, 0.01).name(`rot y Cone`)
+            this.debugFolder.add(this.cone.rotation, 'z', -Math.PI, Math.PI, 0.01).name(`rot z Cone`)
         }
     }
 }
