8fe60b0ee7036cbb6cc81ef46ccef9823d583230
= <rodrigocury@ufu.br>
2021-06-21 16:27:30 -0300

    Added tags & Styling to Projects cards

--- src/javascript/DOM/index.js
+++ src/javascript/DOM/index.js
@@ -1,3 +1,5 @@
+import { Vector2, Vector3 } from 'three'
+
 export default class DOM {
     /**
      * Constructor
@@ -26,15 +28,71 @@ export default class DOM {
             p: "#ffffff"
         }
 
+        this.font = [
+
+        ]
+
         this.getDOMElements()
+        this.setDom3dPosition()
     }
 
     getDOMElements() {
-        this.menuList = document.querySelector('.menu-list')
-        this.menuBtns = [...this.menuList.children]
-        this.homeDiv = document.querySelector('.home')
-        this.content = document.querySelector('.content')
-        console.log(this.menuBtns);
+        this.homeDiv = document.querySelector('.homepage')
+        this.whoami = [...document.querySelectorAll('.whoami-container')]
+        this.projects = document.querySelector('.projects-container')
+        this.imgs = [...document.querySelectorAll('.project-img')]
+        
+        
+        if(this.debug){
+            this.text = [...document.querySelectorAll(".project-description")]
+            this.debugFolder.addColor(this.colors, 'p').onChange(() => {
+                this.text.forEach(child => {
+                    child.style.color = this.colors.p
+                })
+            })
+            this.text.forEach(child => {
+                this.debugFolder.add(child.style , 'font-family', {
+                    mateSC : 'Mate SC',
+                    Caveat : "Caveat",
+                    mSerrat : "Montserrat"
+                })
+            })
+        }
+    }
+
+    setDom3dPosition() {
+        this.firstPositions = [
+            {
+                name: 'homeDiv',
+                element: this.homeDiv,
+                position: new Vector3(0, 2, 0)
+            },
+            {
+                name: 'whoami',
+                element: this.whoami[0],
+                position: new Vector3(0, -2, 0)
+            },
+            {
+                name: 'whatido',
+                element: this.whoami[1],
+                position: new Vector3(0, -6, 0)
+            },
+            {
+                name: 'aboutme',
+                element: this.whoami[2],
+                position: new Vector3(0, -10, 0)
+            }
+
+        ]
+
+        this.secondPositions = [
+            {
+                name: 'projects',
+                element: this.projects,
+                position: new Vector3(0, -35, 0)
+            }
+        ]
+
     }
 
 }
