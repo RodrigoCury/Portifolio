b49de705ded846f71a4687e1a953c546f6b7eeba
Rodrigo Cury <rodrigocury@ufu.br>
2021-07-05 12:47:07 -0300

    RM Draco Loader || Not Worth

--- src/javascript/Utils/Loader.js
+++ src/javascript/Utils/Loader.js
@@ -86,28 +86,9 @@ export default class Resources extends EventEmitter {
                 image.src = _resource.source
             }
         })
-
-        // Draco Loader
-        const dracoLoader = new DRACOLoader()
-        dracoLoader.setDecoderPath('/static/draco/')
-        dracoLoader.setDecoderConfig({ type: 'js' })
-
-        this.loaders.push({
-            name: 'Draco',
-            extentions: ['drc'],
-            action: _resource => {
-                dracoLoader.load(_resource.source, _data => {
-                    this.fileLoadEnd(_resource, _data)
-
-                    DRACOLoader.releaseDecoderModule()
-                })
-            }
-        })
-
         // GLTF Loader
 
         const gltfLoader = new GLTFLoader()
-        gltfLoader.setDRACOLoader(dracoLoader)
 
         this.loaders.push({
             name: 'GLTF',
